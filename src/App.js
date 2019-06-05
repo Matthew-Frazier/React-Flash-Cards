d5a4952db7bc6a604c037e855ac37a5f26d96fe8
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-06-05 08:25:02 -0600

    flash cards are functional as well as the add card and delete, just need to add edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FlashCards from './FlashCards'
+import FlashCardForm from './FlashCardForm'
+import { Container, Header, Button, Segment, } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    cards: [
+      { id: 1, front: "question", back: "answer", },
+      { id: 2, front: "question", back: "answer", },
+      { id: 3, front: "question", back: "answer", },
+      { id: 4, front: "question", back: "answer", },
+    ],
+    // showCard: true,
+    showForm: false,
+  };
+
+  getId = () => {
+    return Math.floor((1 + Math.random()) * 10000);
+  };
+
+  addCard = (cardData) => {
+    let card = { id: this.getId(), ...cardData, }
+    this.setState({ cards: [...this.state.cards, card] })
+  } 
+
+  removeCard = (id) => {
+    const cards = this.state.cards.filter( card => {
+      if (card.id !== id)
+        return card
+    })
+    this.setState({cards, })
+  }
+
+  toggleForm = () => this.setState( {showForm: !this.state.showForm} )
+
+  render () {
+    return (
+      <Container textAlign="center" style={{paddingTop: "15px", letterSpacing: "3px"}}>
+        <Header as="h1">React Flash Cards</Header>
+          <Button onClick={this.toggleForm}>Add A Card</Button>
+        <Segment basic>
+          {this.state.showForm ? <FlashCardForm add={this.addCard} /> : null }
+        </Segment>
+        <Segment>
+          <FlashCards cardsList={this.state.cards} remove={this.removeCard} />
+        </Segment>
+      </Container>
+    )
+  }
 }
 
 export default App;
